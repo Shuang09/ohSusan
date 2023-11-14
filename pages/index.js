12c2750174c02af046dabb5b08eed035a82cd806
Shuhang09 <18126252375@163.com>
2023-11-13 20:16:32 -0500

    Adding css styles on home page

--- pages/index.js
+++ pages/index.js
@@ -4,24 +4,14 @@ import styles from "../styles/home.module.css";
 export default function Home() {
   return (
     <div className={styles.container}>
-
-      <header className={styles.nav}>
-        <ol>
-          <li>
-            github
-            <a></a>
-          </li>
-
-          <li>
-            linkedIn
-            <a></a>
-          </li>
-          <li>
-            Resume
-            <a></a>
-          </li>
-        </ol>
-      </header>
+        <nav class={styles.nav}>
+          <a class= {styles.a_active} href="#home">
+            Home
+          </a>
+          <a href="#linkedIn">linkedIn</a>
+          <a href="#Github">Github</a>
+          <a href="#Contact">Contact</a>
+        </nav>
 
       <main className={styles.main}>
         <Head>
@@ -30,21 +20,34 @@ export default function Home() {
         </Head>
 
         <p className={styles.description_intro}> Hi, I am</p>
-        <h1 className={styles.description_title}>Susan</h1>
+        <h1 className={styles.description_title}>Susan Shuhang Yan</h1>
 
         <p className={styles.description}>
-          description... i am gonna have a long description... Told you a really
-          long one. So I can take up some space here. I will start by telling
-          you my childhood stories. .. Told you a really long one. So I can take
-          up some space here. I will start by telling you my childhood stories.
-          .. Told you a really long one. So I can take up some space here. I
-          will start by telling you my childhood stories. .. Told you a really
-          long one. So I can take up some space here. I will start by telling
-          you my childhood stories. ..
+          A software engineer based in Montreal, passionate about the art of
+          merging technology and creativity. I see myself not just as a coder
+          but as a problem solver, driven by the curiosity to understand the
+          "why" and "how" of every challenge.
+          <br></br>
+          Beyond the world of code, I'm an artist at heart. Drawing is my
+          canvas, and I find joy in exploring the rich interplay of colors and
+          textures. This artistic inclination seeps into my approach to software
+          engineering, where I consider each project a unique masterpiece
+          waiting to be crafted.
+          <br></br>I thrive in the synergy of logic and creativity, where coding
+          becomes an art form. My portfolio reflects not only my technical
+          prowess but also my commitment to innovative solutions and a keen
+          aesthetic sense.
+          <br></br>I am an avid reader. This love for literature enriches my
+          perspective, infusing my work with a depth that goes beyond the
+          surface.
+          <br></br>
+          Join me on this journey where technology meets art, and problems
+          transform into opportunities. Let's create something exceptional
+          together.
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
+          <a href="https://github.com/Shuang09" className={styles.card}>
             <h3>Enginerring Projects &rarr;</h3>
             <p>
               Click this and you will be linked to github project details page
@@ -52,16 +55,29 @@ export default function Home() {
           </a>
 
           <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
+            href="#artProject"
             className={styles.card}
           >
             <h3>Art Projects &rarr;</h3>
             <p>Click this and you will be linked to art project details page</p>
           </a>
 
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Other Projects &rarr;</h3>
+          <a
+            href = "#subDomain - blog"
+            className={styles.card}
+          >
+            <h3>My Blog &rarr;</h3>
             <p>Click this and you will be linked to other pages</p>
+          </a>
+          <a
+            href = "#travelling"
+            className={styles.card}
+          >
+            <h3>Travelling &rarr;</h3>
+            <p>
+              Click this and you will be linked to traveling stuffs haven't
+              figure out how
+            </p>
           </a>
         </div>
       </main>
@@ -75,9 +91,7 @@ export default function Home() {
           github
           <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
-        <a>
-        linkedIn
-        </a>
+        <a>linkedIn</a>
       </footer>
     </div>
   );
